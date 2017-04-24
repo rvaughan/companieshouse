25f9d1713ccdadfa1035e3eed8ecc01f0da554d5
Arjan van Eersel <arjan@vaneersel.eu>
2017-04-24 13:21:29 +0300

    Added root handler

--- example/main.go
+++ example/main.go
@@ -1,3 +1,24 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+	"fmt"
+	"html/template"
+)
+
+func main() {
+	http.Handle("/static", http.FileServer(http.Dir("static")))
+	http.HandleFunc("/favicon.ico", http.NotFound)
+	http.HandleFunc("/", rootHandler)
+
+	http.ListenAndServe(":8000", nil)
+}
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("base.html", "root.html")
+	if err != nil {
+		fmt.Fprintf(w,"Error while parsing template: %s", err.Error())
+	}
+	t.ExecuteTemplate(w, "content", nil)
+}
+
