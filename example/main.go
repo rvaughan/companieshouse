2ba87d91afbcc45eb85e26877378b8ad262564d3
Arjan van Eersel <arjan@vaneersel.eu>
2017-04-25 14:23:03 +0300

    Fixed data bug in example/main.go

--- example/main.go
+++ example/main.go
@@ -86,6 +86,12 @@ func rootHandler(w http.ResponseWriter, r *http.Request) {
 
 func companyHandler(w http.ResponseWriter, r *http.Request) {
 	data := newData()
+	t, err := template.ParseFiles("templates/base.html", "templates/company.html")
+	if err != nil {
+		fmt.Fprintf(w,"Error while parsing template: %s", err.Error())
+		return
+	}
+
 	v := mux.Vars(r)
 	id, ok := v["id"]
 	if !ok {
@@ -96,44 +102,37 @@ func companyHandler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		data.Alerts = append(data.Alerts, Alert{"Error", "alert-danger", err.Error()})
 	} else {
-		data.Data["Company"] = company
+		data.Data["Company"] = *company
 
 		officers, err := company.GetOfficers()
 		if err == nil {
-			data.Data["Officers"] = officers
+			data.Data["Officers"] = *officers
 		} else {
 			log.Println("Error when getting officers:", err)
 		}
 
-
 		filings, err := company.GetFilings()
 		if err == nil {
-			data.Data["Filings"] = filings
+			data.Data["Filings"] = *filings
 		} else {
 			log.Println("Error when getting filings:", err)
 		}
 
 		charges, err := company.GetCharges()
 		if err == nil {
-			data.Data["Charges"] = charges
+			data.Data["Charges"] = *charges
 		} else {
 			log.Println("Error when getting charges:", err)
 		}
 
-
 		insolvencyDetails, err := company.GetInsolvencyDetails()
 		if err == nil {
-			data.Data["InsolvencyDetails"] = insolvencyDetails
+			data.Data["InsolvencyDetails"] = *insolvencyDetails
 		} else {
 			log.Println("Error while getting insolvency details:", err)
 		}
 	}
 
-	t, err := template.ParseFiles("templates/base.html", "templates/company.html")
-	if err != nil {
-		fmt.Fprintf(w,"Error while parsing template: %s", err.Error())
-		return
-	}
 	t.ExecuteTemplate(w, "base", data)
 }
 
